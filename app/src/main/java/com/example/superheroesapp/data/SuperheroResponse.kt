c04a4853ef2df91fa97fbc3848a2622f53ce7190
fregidorsalazar <fregidorsalazar@gmail.com>
2024-06-24 13:38:08 +0200

    Creada llamada a api, y muestra en pantalla con recyclerview

---
+++ app/src/main/java/com/example/superheroesapp/data/SuperheroResponse.kt
@@ -0,0 +1,20 @@
+package com.example.superheroesapp.data
+import com.google.gson.annotations.SerializedName
+data class SuperheroResponse (
+    @SerializedName("response") val response: String,
+    @SerializedName("results-for") val resultsFor: String,
+    @SerializedName("results") val results: List<Superhero>){
+
+
+}
+data class Superhero(
+    @SerializedName("name") val name: String){
+
+    }
+    @SerializedName("image") val image:Image){
+}
+data class Image(
+    @SerializedName("url") val url: String){
+
+}
+
